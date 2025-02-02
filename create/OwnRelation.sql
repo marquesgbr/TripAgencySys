64b801be5fd5af73d7e0bc9680d8137191ce1a80
marquesgbr <gma5@cin.ufpe.br>
2025-02-02 17:02:31 -0300

    feat: Relation "Possui" script

---
+++ create/OwnRelation.sql
@@ -0,0 +1,28 @@
+-- Possui(CodAtividade*, CodPacote*, CNPJFornecedor*)
+-- CodAtividade referencia Atividade(Codigo)
+
+CREATE TABLE Possui (
+    CodAtividade INT,
+    CodPacote INT,
+    CNPJFornecedor VARCHAR2(14),
+    CONSTRAINT possui_pkey PRIMARY KEY (CodAtividade, CodPacote, CNPJFornecedor),
+    CONSTRAINT possui_fkey1 FOREIGN KEY (CodAtividade)
+        REFERENCES Atividade(Codigo),
+    CONSTRAINT possui_fkey2 FOREIGN KEY (CodPacote)
+        REFERENCES Pacote(Codigo),
+    CONSTRAINT possui_fkey3 FOREIGN KEY (CNPJFornecedor)
+        REFERENCES Fornecedor(CNPJ),
+    CONSTRAINT possui_check CHECK (CodAtividade>=0 AND CodPacote>0 AND LENGTH(CNPJFornecedor) = 14)
+);
+
+CREATE SEQUENCE possui_seq
+START WITH 1 INCREMENT BY 2;
+
+INSERT INTO Possui VALUES (1, 1, '12345678901234');
+INSERT INTO Possui VALUES (90, 11, '17190555670023');
+INSERT INTO Possui VALUES (2, possui_seq.nextval, '50787852849859');
+INSERT INTO Possui VALUES (4, 11, '45559011626759');
+INSERT INTO Possui VALUES (possui_seq.nextval, 3, '17190555670023');
+
+
+SELECT * FROM Possui;
