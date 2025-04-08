760e51d3d50d33a007825cfa705abf58d44cb463
marquesgbr <gma5@cin.ufpe.br>
2025-04-07 22:50:24 -0300

    EE2 studies a night before
    Co-authored-by: Matheus Borges <matheusborgesfig@gmail.com>

---
+++ treinoEE2.sql
@@ -0,0 +1,344 @@
+-- SQLBook: Code
+
+------ EE2 23.2
+-- Q1 (nao coloquei os atributos triviais, aqui so tem oq eh importante)
+
+-- Tipos
+
+CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
+    cep VARCHAR2(9),
+    rua VARCHAR(50),
+    numero INT,
+    complemento VARCHAR(50)
+);
+/
+
+CREATE OR REPLACE TYPE tp_telefone_pessoa AS OBJECT(
+    numero VARCHAR2(15)
+);
+/
+
+CREATE OR REPLACE TYPE tp_fones_pessoa AS VARRAY(5) of tp_telefone;
+/ 
+
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT(
+    endereco tp_endereco,
+    telefone tp_fones_pessoa,
+    data_nascimento DATE,
+    nome VARCHAR2(64),
+    cpf VARCHAR2(11)
+) NOT FINAL NOT INSTANTIABLE;
+/
+
+CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
+    cargo_nome VARCHAR2(20),
+    salario NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
+    cargo REF tp_cargo,
+    supervisor REF tp_funcionario
+);
+/
+
+CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa(
+    num_atendimentos INT
+);
+/
+
+CREATE OR REPLACE TYPE tp_carro AS OBJECT (
+    proprietario REF tp_cliente,
+    placa VARCHAR2(7),
+    marca VARCHAR2(30),
+    modelo VARCHAR2(30),
+    cor VARCHAR2(25)
+);
+/
+
+CREATE OR REPLACE TYPE tp_telefone_empresa AS OBJECT(
+    numero VARCHAR2(15)
+);
+/
+
+CREATE OR REPLACE TYPE tp_fones_empresa AS VARRAY(6) OF tp_telefone_empresa;
+/
+
+CREATE OR REPLACE TYPE tp_fornecedor AS OBJECT(
+    telefones tp_fones_empresa,
+    cnpj VARCHAR2(14),
+    nome_empresa VARCHAR2(30)
+);
+/
+
+CREATE OR REPLACE TYPE tp_produto AS OBJECT(
+    fornecedor REF tp_fornecedor,
+    nome VARCHAR2(20),
+    preco NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tp_nt_produtos AS TABLE OF tp_produto;
+/
+
+CREATE OR REPLACE TYPE tp_servico AS OBJECT(
+    produtos tp_nt_produtos,
+    nome VARCHAR2(30),
+    valor NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tp_maquina AS OBJECT(
+    codigo VARCHAR2(7),
+    fabricante VARCHAR2(30),
+    nome VARCHAR2(30)
+);
+/
+
+CREATE OR REPLACE TYPE tp_atendimento AS OBJECT(
+    funcionario REF tp_funcionario,
+    carro REF tp_carro,
+    servico REF tp_servico,
+    maquina REF tp_maquina,
+    custo_total NUMBER,
+    data DATE
+);
+/
+
+
+-- Tabelas
+
+CREATE TABLE tb_cargo of tp_cargo (
+    cargo_nome PRIMARY KEY,
+    CONSTRAINT salario_minimo CHECK (salario >= 1400) 
+);
+
+CREATE TABLE tb_funcionario OF tp_funcionario (
+    supervisor WITH ROWID REFERENCES tb_funcionario,
+    cargo WITH ROWID REFERENCES tb_cargo,
+    cpf PRIMARY KEY
+);
+
+CREATE TABLE tb_cliente OF tp_cliente (
+    cpf PRIMARY KEY 
+);
+
+CREATE TABLE tb_carro OF tp_carro (
+    proprietario WITH ROWID REFERENCES tb_cliente,
+    placa PRIMARY KEY
+);
+
+
+CREATE TABLE tb_fornecedor OF tp_fornecedor (
+    cnpj PRIMARY KEY
+);
+
+CREATE TABLE tb_servico OF tp_servico (
+    codigo PRIMARY KEY
+) NESTED TABLE produtos STORE AS nt_produtos;
+
+
+CREATE TABLE tb_maquina OF tp_maquina (
+    codigo PRIMARY KEY
+);
+
+CREATE TABLE tb_atendimento OF tp_atendimento (
+    funcionario WITH ROWID REFERENCES tb_funcionario,
+    carro WITH ROWID REFERENCES tb_carro,
+    servico WITH ROWID REFERENCES tb_servico,
+    maquina WITH ROWID REFERENCES tb_maquina,
+    custo_total NOT NULL,
+    data NOT NULL
+);
+
+
+-- Q2
+
+-- a) Qual o serviço mais comum na oficina?
+SELECT
+    s.nome,
+    COUNT(*) as ocorrencias
+FROM tb_servico s
+JOIN tb_atendimento a ON DEREF(a.servico).codigo = s.codigo
+GROUP BY s.nome
+ORDER BY ocorrencias DESC
+FETCH FIRST 1 ROWS ONLY;
+
+
+-- b) Qual a média salarial dos funcionários da oficina?
+-- Minha resposta
+SELECT AVG(c.salario) as salario_medio
+FROM tb_cargo c
+JOIN tb_funcionario f ON DEREF(f.cargo).cargo_nome = c.cargo_nome;
+-- Monitoria
+SELECT AVG((DEREF(f.cargo)).salario) as media_salarial
+FROM tb_funcionario f
+WHERE DEREF(f.cargo) IS NOT NULL;
+
+
+-- c) Qual o funcionário que efetuou mais serviços?
+SELECT f.cpf, f.nome, COUNT(*) as num_servicos
+FROM tb_funcionario f
+JOIN tb_atendimento a ON DEREF(a.funcionario).cpf = f.cpf
+GROUP BY f.cpf, f.nome
+ORDER BY num_servicos DESC
+FETCH FIRST 1 ROWS ONLY;
+
+-- i) Quais os produtos utilizados no 'Serviço de Freios' e o preço de cada um deles?
+-- Consulta a nested table
+SELECT p.nome, p.preco FROM TABLE (
+    SELECT s.produtos FROM tb_servico s
+    WHERE s.nome LIKE 'Serviço de Freios'
+) p;
+
+-- j) Mostre quanto cada cliente gastou no total, ou seja, contando 
+-- com todos os atendimentos realizados, para cada cliente.
+
+SELECT DEREF(c.proprietario).nome as proprietario, SUM(a.custo_total) as total_gasto
+FROM tb_carro c
+INNER JOIN tb_atendimento a ON DEREF(a.carro).placa = c.placa
+GROUP BY proprietario
+ORDER BY total_gasto DESC;
+
+-- k) Qual foi o atendimento mais caro? Selecione o serviço que foi 
+-- feito, a data, o funcionário que fez o serviço, além do custo e a 
+-- placa e modelo do carro em que esse serviço foi feito.
+
+SELECT 
+    DEREF(a.servico).nome as Servico, 
+    a.data as DataAtendimento,
+    DEREF(a.funcionario).nome as Funcionario,
+    a.custo_total as MaiorValor,
+    DEREF(a.carro).placa as PlacaCarro,
+    DEREF(a.carro).modelo as ModeloCarro 
+FROM tb_atendimento a
+WHERE a.custo_total = (SELECT MAX(custo_total) FROM tb_atendimento);
+
+
+-- l) Qual a empresa que fornece 'Bateria' para o serviço de 
+-- 'Troca de Bateria' da Oficina? Mostre o nome da empresa e seus telefones.
+
+SELECT 
+    DEREF(p.fornecedor).nome_empresa as NomeEmpresa,
+    t AS FonesEmpresa
+    FROM TABLE (
+        SELECT s.produtos FROM tb_servico s
+        WHERE s.nome LIKE 'Troca de Bateria'
+    ) p, tb_fornecedor F, TABLE(F.telefones) t
+WHERE p.nome LIKE 'Bateria'
+AND f.nome_empresa = NomeEmpresa;
+
+
+-- m) Quais os serviços que utilizam a máquina 'Elevador Automotivo'?
+-- Minha Resposta (enxuta)
+SELECT DISTINCT DEREF(a.servico).nome as NomeServico
+FROM tb_atendimento a
+WHERE DEREF(a.maquina).nome = 'Elevador Automotivo';
+-- Monitoria (redundante, mas correta)
+SELECT S.nome
+FROM tb_servico S
+INNER JOIN tb_atendimento A ON S.codigo = DEREF(A.servico).codigo
+INNER JOIN tb_maquina M ON DEREF(A.maquina).codigo = M.codigo
+WHERE M.nome = 'Elevador Automotivo'
+GROUP BY S.nome;
+
+
+-- d) Quais os nomes dos funcionários que não possuem supervisores? Quais os cargos deles?
+SELECT f.nome as Nome, DEREF(f.cargo).nome_cargo as Cargo
+FROM tb_funcionario f
+WHERE f.supervisor IS NULL;
+
+
+-- f) Qual a marca de carro mais frequente na oficina? Qual é a cor mais comum?
+SELECT 
+    c1.marca as Marca,
+    COUNT(*) as CountMarca
+FROM tb_carro c1
+GROUP BY Marca
+HAVING COUNT(*) = (SELECT MAX(COUNT(c2.marca)) FROM tb_carro c2 GROUP BY c2.marca);
+
+
+-- e) Quais os modelos dos carros que possuem a cor ‘VERMELHO’?
+SELECT c.modelo
+FROM tb_carro c
+WHERE c.cor = 'VERMELHO';
+
+-- h) Consulte o nome, cpf e os números de celular dos funcionários que têm o cargo de ‘Mecânico’.
+SELECT f.nome, f.cpf, fones.*
+FROM tb_funcionario f, TABLE(f.telefone) fones
+WHERE DEREF(f.cargo).cargo_nome = 'Mecânico'; 
+
+
+-- Q3
+
+-- e) Escreva o TYPE BODY de tp_cliente, escrevendo a função ‘exibirinformacoes’, que deve 
+-- exibir as informações de um cliente. 
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
+    cpf VARCHAR2(14), 
+    nome VARCHAR2(30), 
+    data_nascimento DATE, 
+    MEMBER PROCEDURE exibirInformacoes(SELF tp_pessoa)
+) NOT FINAL NOT INSTANTIABLE;
+/
+
+
+CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa ( 
+    quantidade_de_compras INTEGER, 
+    OVERRIDING MEMBER PROCEDURE exibirInformacoes(SELF tp_cliente)
+);
+/
+
+CREATE OR REPLACE TYPE BODY tp_cliente AS
+    OVERRIDING MEMBER PROCEDURE exibirInformacoes(SELF tp_cliente) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('CPF: ' || SELF.cpf);
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || SELF.nome);
+        DBMS_OUTPUT.PUT_LINE('Data de Nascimento: ' || SELF.data_nascimento);
+        DBMS_OUTPUT.PUT_LINE('Qtde de Compras: ' || SELF.quantidade_de_compras);
+    END;
+END;
+
+
+-- f) Crie uma função que, dado o CPF do cliente, retorne quanto ele gastou, levando em 
+-- conta todas as compras que ele fez.CREATE OR REPLACE 
+FUNCTION calcularGasto(cpf IN VARCHAR) RETURN NUMBER IS
+BEGIN
+    SELECT SUM(DEREF(c.pedido).valor)
+    INTO v_total_gasto
+    FROM tb_compra c
+    WHERE DEREF(c.cliente).cpf = cpf;
+
+    RETURN v_total_gasto;
+END calcularGasto;
+
+-- c) Quais os ingredientes do ‘Brownie Recheado’? Informe os nomes dos ingredientes e seus fornecedores.
+SELECT i.nome, DEREF(i.empresa).nome as NomeEmpresa 
+FROM TABLE (
+    SELECT d.ingredientes 
+    FROM tb_doces d WHERE d.nome = 'Brownie Recheado'
+    ) i;
+
+CREATE TABLE tb_pedido OF tp_pedido (
+    codigo_pedido PRIMARY KEY, 
+    valor NOT NULL, 
+    numero_itens NOT NULL
+);
+
+CREATE TABLE tb_contem OF tp_contem (
+    pedido WITH ROWID REFERENCES tb_pedido, 
+    doce WITH ROWID REFERENCES tb_doce, 
+    quantidade_doces NOT NULL
+);
+
+
+-- b) Qual o doce mais vendido pela doceria? Informe o seu código, nome e quantidade vendida.
+
+SELECT 
+    DEREF(c.doce).codigo as CodigoDoce, 
+    DEREF(c.doce).nome as NomeDoce,
+    SUM(c.quantidade_doces) as QuantidadeVendida 
+FROM tb_contem c 
+GROUP BY c.doce  
+HAVING SUM(c.quantidade_doces) = (
+    SELECT MAX(SUM(c.quantidade_doces)) FROM tb_contem c GROUP BY c.doce
+);
